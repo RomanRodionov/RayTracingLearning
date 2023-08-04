d2889467c4d70519370e067d43516985626bc2bd
Roman <rodionovv22@gmail.com>
2023-08-05 01:50:33 +0300

    add multithreading

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "main.h"
 
-color ray_color(const Ray& ray, const Object& scene, int depth, shared_ptr<SkyBox> skybox)
+color ray_color(const Ray& ray, shared_ptr<Object> scene, int depth, shared_ptr<SkyBox> skybox)
 {
     hit_record hit;
 
@@ -9,7 +9,7 @@ color ray_color(const Ray& ray, const Object& scene, int depth, shared_ptr<SkyBo
         return color(0.0, 0.0, 0.0);
     }
 
-    if (scene.hit(ray, EPS, INF, hit))
+    if (scene->hit(ray, EPS, INF, hit))
     {
         Ray scattered;
         color attenuation;
@@ -26,11 +26,31 @@ color ray_color(const Ray& ray, const Object& scene, int depth, shared_ptr<SkyBo
     */
 }
 
+std::mutex mtx;
+
+void compute_pixel(int i, int j,
+                   int height, int width, 
+                   shared_ptr<Image> image,
+                   const SceneData& data)
+{
+    color pixel_color(0, 0, 0);
+    for (int s = 0; s < SAMPLES_PER_PIXEL; ++s)
+    {
+        double v = (i + rand_double()) / (IMG_HEIGHT - 1);
+        double u = (j + rand_double()) / (IMG_WIDTH - 1);
+        Ray ray = data.camera->get_ray(u, v);
+        pixel_color += ray_color(ray, data.scene, MAX_DEPTH, data.sky_box);
+    }
+    mtx.lock();
+    image->draw_pixel(IMG_HEIGHT - i - 1, j, pixel_color, SAMPLES_PER_PIXEL);
+    mtx.unlock();
+}
+
 int main(int, char**){
     std::srand(100);
 
     std::string filename = OUTPUT_FILE;
-    Image image(IMG_HEIGHT, IMG_WIDTH);
+    shared_ptr<Image> image = make_shared<Image>(IMG_HEIGHT, IMG_WIDTH);
 
     const double aspect_ratio = (double) IMG_WIDTH / (double) IMG_HEIGHT;
 
@@ -43,43 +63,48 @@ int main(int, char**){
 
     auto sky_box = make_shared<SkyBox>(make_shared<Texture>(make_shared<Image>(SKYBOX_FILE)));
 
-    ObjectsList scene;
-    scene.add(make_shared<Sphere>(point3(0, 0, -3.0), 1.0, mat_center));
-    scene.add(make_shared<Sphere>(point3(-2.0, 0, -3.0), 1.0, glass1));
-    scene.add(make_shared<Sphere>(point3(-2.0, 0, -3.0), -0.9, glass1));
-    scene.add(make_shared<Sphere>(point3(2.0, 0, -3.0), 1.0, mat_right));
-    scene.add(make_shared<Sphere>(point3(0, -10001.0, -3), 10000.0, mat_ground));
-    scene.add(make_shared<Sphere>(point3(1.0, -0.75, -1.5), 0.25, mat_small));
-    scene.add(make_shared<Sphere>(point3(-0.5, -0.6, -2), 0.4, glass2));
+    shared_ptr<ObjectsList> scene = make_shared<ObjectsList>();
+    scene->add(make_shared<Sphere>(point3(0, 0, -3.0), 1.0, mat_center));
+    scene->add(make_shared<Sphere>(point3(-2.0, 0, -3.0), 1.0, glass1));
+    scene->add(make_shared<Sphere>(point3(-2.0, 0, -3.0), -0.9, glass1));
+    scene->add(make_shared<Sphere>(point3(2.0, 0, -3.0), 1.0, mat_right));
+    scene->add(make_shared<Sphere>(point3(0, -10001.0, -3), 10000.0, mat_ground));
+    scene->add(make_shared<Sphere>(point3(1.0, -0.75, -1.5), 0.25, mat_small));
+    scene->add(make_shared<Sphere>(point3(-0.5, -0.6, -2), 0.4, glass2));
 
     double focus_dist = (LOOK_FROM - LOOK_AT).length();
 
-    Camera camera(LOOK_FROM, LOOK_AT, VIEW_UP, FOV, aspect_ratio, APERTURE, focus_dist);
+    shared_ptr<Camera> camera = make_shared<Camera>(LOOK_FROM, LOOK_AT, VIEW_UP, FOV, aspect_ratio, APERTURE, focus_dist);
     
+    SceneData scene_data = {scene, camera, sky_box};
+
     std::cout << "Rendering in progress: 000%";
     int pixel_count = 0, image_size = IMG_HEIGHT * IMG_WIDTH;
+
+    std::thread threads[THREADS_NUM];
+
     for (int i = 0; i < IMG_HEIGHT; ++i)
     {
-        for (int j = 0; j < IMG_WIDTH; ++j)
+        for (int j = 0; j < IMG_WIDTH / THREADS_NUM + 1; ++j)
         {
-            color pixel_color(0, 0, 0);
-            for (int s = 0; s < SAMPLES_PER_PIXEL; ++s)
+            for (int t = 0; t < THREADS_NUM && j * THREADS_NUM + t < IMG_WIDTH; ++t)
             {
-                double v = (i + rand_double()) / (IMG_HEIGHT - 1);
-                double u = (j + rand_double()) / (IMG_WIDTH - 1);
-                Ray ray = camera.get_ray(u, v);
-                pixel_color += ray_color(ray, scene, MAX_DEPTH, sky_box);
+                threads[t] = std::thread(compute_pixel, i, j * THREADS_NUM + t, IMG_HEIGHT, IMG_WIDTH, image, scene_data);
             }
-            image.draw_pixel(IMG_HEIGHT - i - 1, j, pixel_color, SAMPLES_PER_PIXEL);
+            for (int t = 0; t < THREADS_NUM && j * THREADS_NUM + t < IMG_WIDTH; ++t)
+            {
+                threads[t].join();
+                pixel_count++;
+            }
             
-            pixel_count++;
             double progress = (int)((double) pixel_count / image_size * 100.0);
             std::ostringstream ss;
             ss << std::setw(3) << std::setfill('0') << progress;
             std::string progress_str = ss.str();
             std::cout << "\b\b\b\b" << progress_str << "%";
+            
         }
     }
-    image.save_to_png(filename);
+    image->save_to_png(filename);
     std::cout << std::endl << "File " << filename << " has been saved" << std::endl;;
 }
--- main.h
+++ main.h
@@ -7,6 +7,9 @@
 #include <string>
 #include <limits>
 
+#include <thread>
+#include <mutex>
+
 #include <sstream>
 #include <iomanip>
 
@@ -26,15 +29,23 @@
 
 #define OUTPUT_FILE "output.png"
 #define SKYBOX_FILE "./data/cubemap.png"
-#define IMG_WIDTH (640 * 1)
-#define IMG_HEIGHT (360 * 1)
-#define SAMPLES_PER_PIXEL 200
+#define IMG_WIDTH (640 * 3)
+#define IMG_HEIGHT (360 * 3)
+#define SAMPLES_PER_PIXEL 300
 #define FOV 90
 #define MAX_DEPTH 50
 #define LOOK_FROM point3(-2, 1, 0.5)
 #define LOOK_AT point3(0, 0,-3)
 #define VIEW_UP point3(0, 1, 0)
 #define APERTURE 0.1
+#define THREADS_NUM 4
+
+struct SceneData
+{
+    shared_ptr<ObjectsList> scene; 
+    shared_ptr<Camera> camera;
+    shared_ptr<SkyBox> sky_box;
+};
 
 color ray_color(const Ray& ray, const Object& obj, int depth, shared_ptr<SkyBox> skybox);
 
